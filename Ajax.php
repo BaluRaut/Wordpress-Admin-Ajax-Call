4fa8e4223079a4b9c76bb4aa2423c51feb8ae62e
Balasaheb <balsraut@gmail.com>
2015-02-12 11:20:29 +0530

    Update Ajax.php

--- Ajax.php
+++ Ajax.php
@@ -12,3 +12,20 @@ function my_action_callback() {
 
 	wp_die(); // this is required to terminate immediately and return a proper response
 }
+
+
+// Here is functgion how to calll the wordpress Aajx
+<script>
+var data = {
+   'action': 'my_action',
+   'cell_no': $("#mobile").val(),
+   'from_page':'<?php echo site_url();?>'
+  };
+
+  // since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
+  jQuery.post(ajaxurl, data, function(response) {
+  
+  
+  });
+  </script>
+  
